9fd8147847f99f29d08f0910dc6284fdcbbef153
943432216 <35713053+943432216@users.noreply.github.com>
2018-02-04 18:58:02 +0800

    zhang
    动态新闻修改

--- info2-detail.php
+++ info2-detail.php
@@ -4,32 +4,73 @@ include($site_root_path.'/inc/set/ext_var.php');
 include($site_root_path.'/inc/fun/mysql.php');
 include($site_root_path.'/inc/function.php');
 include($site_root_path.'/inc/common.php');
-include($site_root_path.'/inc/lib/info2/detail.php');
-$CateId=(int)$info_row['CateId'];
-if($CateId){
-	$cur_cate=$db->get_one('info2_category',"CateId='$CateId'");
-}
-$pageName='info2';
-$banner=$db->get_one('ad',"AId='7'");
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta charset="utf-8">
-<link rel="stylesheet" href="/css/global.css" />
-<link rel="stylesheet" href="/css/lib.css" />
-<link rel="stylesheet" href="/css/style.css" />
-<?=seo_meta_as_lang($info_row)?>
-</head>
-<body>
-<?php include($site_root_path.'/inc/header.php');?>
-<div class="clear"></div>
-<?php include($site_root_path.'/inc/banner.php');?>
-<?php include($site_root_path.'/inc/info2cate.php');?>
-<div class="w1000 info-detail">
-	<?=$info_detail;?>
-</div>
 
-<?php  include($site_root_path.'/inc/footer.php');?>
-</body>
+$content=$db->get_one('info2_contents',"InfoId='29'");
+?>
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width,user-scalable=no" />
+		<title>心宝药业</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css" />
+		<link rel="stylesheet" type="text/css" href="css/dome1.css" />
+		<script type="text/javascript">
+			(function() {
+				var html = document.documentElement;
+				var w = html.getBoundingClientRect().width;
+				html.style.fontSize = w / 15 + 'px';
+			})();
+		</script>
+	</head>
+
+	<body>
+		<div class="xb_box">
+			<div class="i_ul">
+				<?php include('navigate.php'); ?>
+			</div>
+			<header>
+				<div class="header_logo"></div>
+				<h1 class="header_con">公司动态</h1>
+				<div class="header_nav"></div>
+			</header>
+			
+			<div class="detail_box float width">
+				<div class="logob"></div>			
+			</div>
+			<section class="float width">
+				<div class="detail_box float width">
+					<?=$db->get_value('info_contents', "InfoId='208'", 'Contents');?>
+				</div>
+			</section>
+			<div class="blogroll">
+				<div class="blogroll_left">友情链接</div>
+				<div class="blogroll_right">
+					<a href="http://www.360kad.com/product/575808.shtml">龟鹿补肾片-康爱多网上药店</a>
+					<a href="http://www.gdda.gov.cn/publicfiles/business/htmlfiles/jsjzz/index.htm">广东省食品药品监督管理总局</a>
+					<a href="http://www.sda.gov.cn/WS01/CL0001/">国家食品药品监督管理总局</a>
+				</div>
+			</div>
+			<div class="width float">
+				<iframe src="footer.html" width="100%" height="100px" frameborder="0" scrolling="no"></iframe>
+			</div>
+
+		</div>
+	</body>
+	<script src="js/jquery-2.1.1.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="js/unslider.min.js" type="text/javascript" charset="utf-8"></script>
+	<script type="text/javascript">
+		$(function() {
+			$('.staOne').children('ul').slideUp();
+			$('.header_nav').click(function() {
+				$('.i_ul').toggle(500);
+			})
+			$('.staOne').click(function() {
+				$('.staOne').children('ul').slideUp();
+				$(this).children('ul').slideToggle();
+			})
+		})
+	</script>
+
 </html>
