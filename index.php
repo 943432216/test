376f86c5854224511edc39817a4a3cf4324b2e50
943432216 <35713053+943432216@users.noreply.github.com>
2018-02-04 14:30:54 +0800

    zhang
    将导航栏单独切出来做一个页面

--- index.php
+++ index.php
@@ -38,70 +38,7 @@ $company_state = json_encode($company_state, JSON_UNESCAPED_UNICODE);
 	<body>
 		<div class="xb_box">
 			<div class="i_ul">
-				<ul>
-					<li style="background: #B60005;">
-						<a href="index.html">首页</a>
-					</li>
-					<li class="staOne">
-						<a href="javascript:;">关于心宝</a>
-						<ul class="i_uls">
-							<?php
-							if(!empty($art_group[1])){
-								foreach((array)$art_group[1] as $item){
-							?>
-							<li>
-								<a href="<?=get_url('article',$item)?>"><?=$item['Title']?></a>
-							</li>
-							<?php }}?>
-						</ul>
-					</li>
-					<li class="staOne">
-						<a href="javascript:;">产品中心</a>
-						<ul class="i_uls">
-							<?php
-							if(!empty($product_cate)){
-								foreach((array)$product_cate as $item){
-							?>
-							<li>
-								<a href="<?=get_url('product_category',$item)?>"><?=$item['Category']?></a>
-							</li>
-							<?php }}?>
-						</ul>
-					</li>
-					<li class="staOne">
-						<a href="javascript:;">心肾同治</a>
-						<ul class="i_uls">
-							<?php
-							if(!empty($info2_cate)){
-								foreach((array)$info2_cate as $item){
-							?>
-							<li>
-								<a href="<?=get_url('info2_category',$item)?>"><?=$item['Category']?></a>
-							</li>
-							<?php }}?>
-						</ul>
-					</li>
-					<li class="staOne">
-						<a href="javascript:;">最新动态</a>
-						<ul class="i_uls">
-							<?php
-							if(!empty($info_cate)){
-								foreach((array)$info_cate as $item){
-									if($item['CateId'] == 7){continue;}
-							?>
-							<li>
-								<a href="<?=get_url('info_category',$item)?>"><?=$item['Category']?></a>
-							</li>
-							<?php }}?>
-						</ul>
-					</li>
-					<li>
-						<a href="/contact.php?AId=11">联系心宝</a>
-					</li>
-					<li>
-						<a href="https://sso.jingoal.com/#/login">员工登录</a>
-					</li>
-				</ul>
+				<?php include('navigate.php'); ?>
 			</div>
 			<header>
 				<div class="header_logo"></div>
